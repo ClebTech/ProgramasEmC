1edcb2685897045861ee5f9bb5bc052dbf9cec93
Clebson Santos <67807818+ClebTech@users.noreply.github.com>
2023-09-10 21:40:29 -0300

    criei uma arquivo de texto

---
+++ teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    printf("Hello world");
+    printf("\n Teste... aprendendo a mexer com repositorios no github.");
+
+    
+    return 0;
+}
